04964deb6adabea6ed8aff7158f33c9714066e4c
Alexander Gaura <agaura01@gmail.com>
2025-03-30 14:12:56 -0700

    fix imports again

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
-import { EffectComposer } from 'https://unpkg.com/three@0.162.0/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'https://unpkg.com/three@0.162.0/examples/jsm/postprocessing/RenderPass.js';
-import { ShaderPass } from 'https://unpkg.com/three@0.162.0/examples/jsm/postprocessing/ShaderPass.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
 
 export async function addShaderOverlay(scene, camera, renderer, uniforms, vertex, fragment) {
 
